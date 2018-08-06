a74fd8406c1999e98ae883ff28a9bf527df4329f
Sal Mancuso <smancuso@stanford.edu>
2018-08-05 22:37:56 -0700

    Made several modifications to make the script more efficient.

--- bot.py
+++ bot.py
@@ -1,19 +1,22 @@
 import tweepy
 import feedparser
 import tinyurl
-import random
 import csv
-import datetime
-import re
 import time
+import datetime
+import random
+import sys
+import re
+
 
 def randomDice():
     threshold = 30
-    roll =(int(random.uniform(1, 100)))
+    roll = (int(random.uniform(1, 100)))
     if roll < int(threshold):
         return True
     else:
         return False
+
 
 def getCreds():
     with open('mycreds.csv', 'r') as credsRaw:
@@ -22,11 +25,9 @@ def getCreds():
 
 
 #### TWITTER API INTERFACE, ADD YOUR KEYS AND TOKENS
-#### YOU WILL NEED TO GET THESE KEYS AND TOKENS
-#### AT http://apps.twitter.com
 def tweetPoster(tweetString):
     # Consumer keys and access tokens, used for OAuth
-    creds=getCreds()
+    creds = getCreds()
     consumer_key = creds[0]
     consumer_secret = creds[0]
     access_token = creds[0]
@@ -39,63 +40,94 @@ def tweetPoster(tweetString):
     # Creation of the actual interface, using authentication
     api = tweepy.API(auth)
     # Sample method, used to update a status
-    api.update_status(status = tweetString)
+    # api.update_status(status = tweetString)
+    geos = [[37.427621, -122.161944], [37.793372, -122.39711], [37.334308, -121.890445], [38.897691, -77.036488],
+            [38.889804, -77.009185], [40.762374, -73.973912]]
+    cords = random.choice(geos)
+    latitude = cords[0]
+    longitude = cords[1]
+    api.update_status(status=tweetString, lat=latitude, long=longitude)
+
 
 #### CLEAN UP SERIAL NUMBER FOR TRACKING
 def charcterCleaner(dataString):
-    dataString = re.sub('[^A-Za-z0-9]+', '', dataString)
+    dataString = re.sub('[^A-Za-z0-9]+', '', str(dataString))
     return dataString
+
 
 #### TWEET PROCESSING
 def twitterTweetBot():
+    tweetRssLog = []
     #### CHANGE THE NUMBER OF TWEETS TO POST PER CYCLE
     tweetNumbToPost = 1
-
-    tweetRssLog = []
-    with open('tweetBotLogger.csv', 'rb') as tweetLog:
+    with open('/root/twitterBot/tweetBotLogger.csv', 'rb') as tweetLog:
         tweetLogFile = csv.reader(tweetLog, delimiter=',', quotechar='"')
         for eachRow in tweetLogFile:
             tweetRssLog.append(eachRow[0])
 
-    with open('tweetData.csv', 'rb') as rssStuff:
-        rssFeedData = csv.reader(rssStuff, delimiter=',', quotechar='"')
-        rssFeedData.next()
-        tweetLimitCount = 0
-        for feedRow in rssFeedData:
-            RssFeedURL =  feedRow[1]
-            RssFeedHashTag = feedRow[0]
-            getRss = feedparser.parse(RssFeedURL)
-            for feed in getRss.entries:
-                rssFeedTitle = feed.title
-                rssFeedLinkURL = feed.link
-                rssSerialNumber = charcterCleaner(feed.link)[8:48]
-                if rssSerialNumber in tweetRssLog:
-                    None
-                else:
-                    with open('tweetBotLogger.csv', 'a') as tweetLog:
-                        tweetLogFile = csv.writer(tweetLog, delimiter=',', quotechar='"')
-                        tweetLogFile.writerow([rssSerialNumber])
-                        passString = rssFeedTitle + " " + tinyurl.create_one(rssFeedLinkURL) + " " + RssFeedHashTag
-                        if len(passString)>280:
-                            None
-                        else:
-                            if tweetLimitCount <= tweetNumbToPost:
-                                tweetPoster(passString)
-                                tweetLimitCount = tweetLimitCount + 1
-                            else:
-                                None
+        tweetDict = {"http://feeds.feedburner.com/TechCrunchIT": "#tech",
+                     "http://feeds.feedburner.com/TechCrunch/startups": "#startup",
+                     "http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml": "#tech",
+                     "https://www.wired.com/feed/category/security/latest/rss": "#security",
+                     "https://www.wired.com/feed/rss": "#tech #news",
+                     "https://www.wired.com/feed/category/science/latest/rss": "#science",
+                     "https://www.wired.com/feed/category/business/latest/rss": "#tech #business",
+                     "http://feeds.arstechnica.com/arstechnica/technology-lab": "#tech",
+                     "http://feeds.arstechnica.com/arstechnica/business": "#tech #business",
+                     "http://feeds.arstechnica.com/arstechnica/security": "#security #hacktivism",
+                     "http://feeds.arstechnica.com/arstechnica/tech-policy": "#tech #law"}
+
+    feedRow = (random.choice(list(tweetDict.items())))
+    RssFeedURL = feedRow[0]
+    RssFeedHashTag = feedRow[1]
+    tweetLimitCount = 0
+    getRss = feedparser.parse(RssFeedURL)
+    for feed in getRss.entries:
+        rssFeedTitle = feed.title
+        rssFeedLinkURL = feed.link
+        rssSerialNumber = charcterCleaner(feed.link)[8:48]
+        if rssSerialNumber not in tweetRssLog:
+            print rssSerialNumber
+            if tweetLimitCount < tweetNumbToPost:
+                passString = rssFeedTitle + " " + tinyurl.create_one(rssFeedLinkURL) + " " + RssFeedHashTag
+                tweetPoster(passString)
+                tweetLimitCount += 1
+                with open('/root/twitterBot/tweetBotLogger.csv', 'a') as tweetLog:
+                    tweetLogFile = csv.writer(tweetLog, delimiter=',', quotechar='"')
+                    tweetLogFile.writerow([rssSerialNumber])
+            else:
+                None
 
 
-#### HOURS TO OPERATE TWITTER BOT
-#### CHANGE THE HOURS TO CHANGE POSTING WITHIN TIMES
-#### THIS IS POSTING BETWEEN 6AM & 9PM
-currentHour = datetime.datetime.now().hour
-#### CHANGE THE START AND END HOUR FOR EXAMPLE
-#### 5 IS 5 AM WHILE 20 IS 8PM BASED ON THE 24
-#### HOUR CLOCK
-startTime = 6  # Start at 6 am
-endTime = 21  # end at 9pm
-if currentHour >= startTime and currentHour <= endTime:
-    if randomDice == True:
-        time.sleep(int(random.uniform(1, 30)))
-        twitterTweetBot()
+if __name__ == "__main__":
+    #### CHANGE THE START AND END HOUR FOR EXAMPLE
+    #### 5 IS 5 AM WHILE 20 IS 8PM BASED ON THE 24
+    #### HOUR CLOCK
+    startTime = 6  # Start at 6 am
+    endTime = 21  # end at 9pm
+
+    randomNumber = random.randint(0, 55)
+    print randomNumber
+    if randomNumber <= 38:
+        sleepTime = randomNumber * 60
+        currentHour = datetime.datetime.now().hour
+        dayOfTheWeek = datetime.datetime.today().weekday()
+        print ("the day of the week is {}".format(dayOfTheWeek))
+        print ("the current hours is {}".format(currentHour))
+        if dayOfTheWeek > 4 and dayOfTheWeek <= 6:
+            # print ("weekend")
+            sys.exit()
+            None
+        else:
+            if currentHour >= startTime and currentHour <= endTime:
+                print ("Weekday")
+                # print ("""Sleep time {}""".format(sleepTime))
+                time.sleep(sleepTime)
+                twitterTweetBot()
+            else:
+                sys.exit()
+                None
+    else:
+        sys.exit()
+        None
+
--- sandbox3.py
+++
@@ -1,7 +0,0 @@
-import re
-
-testString = """This * ( the new Begnning (#(#($*!($@*!$)._>"""
-
-testString = re.sub('[^A-Za-z0-9]+', '', testString)
-
-print testString
