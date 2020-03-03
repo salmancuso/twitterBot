15c0bb2b72f78590f5eed33c4e513509da91d648
Sal Mancuso <smancuso@stanford.edu>
2020-03-02 21:17:52 -0800

    Fixed the bot... I think

--- sandbox3.py
+++ sandbox3.py
@@ -8,7 +8,6 @@ def pwdDir ():
 tweetRssLog = []
 with open(str('{}/tweetBotLogger.csv'.format(pwdDir())), 'r') as tweetLog:
     tweetLogFile = csv.reader(tweetLog, delimiter=',', quotechar='"')
-    print (tweetLogFile)
     for eachRow in tweetLogFile:
         tweetRssLog.append(eachRow[0])
 
