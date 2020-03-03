cf3c6b0803fac65d4c7160baff343980b8699dec
Sal Mancuso <smancuso@stanford.edu>
2020-03-02 22:29:30 -0800

    Testing

--- bot.py
+++ bot.py
@@ -63,6 +63,7 @@ def charcterCleaner(dataString):
 
 #### TWEET PROCESSING
 def twitterTweetBot():
+    print("starting post")
     tweetRssLog = []
     #### CHANGE THE NUMBER OF TWEETS TO POST PER CYCLE
     tweetNumbToPost = 1
@@ -71,38 +72,39 @@ def twitterTweetBot():
         for eachRow in tweetLogFile:
             tweetRssLog.append(eachRow)
         # print(tweetRssLog)
+    
 
-        tweetDict = {"http://feeds.feedburner.com/TechCrunchIT": "#tech",
-                     "http://feeds.feedburner.com/TechCrunch/startups": "#startup",
-                     "http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml": "#tech",
-                     "https://www.wired.com/feed/category/security/latest/rss": "#security",
-                     "https://www.wired.com/feed/rss": "#tech #news",
-                     "https://www.wired.com/feed/category/science/latest/rss": "#science",
-                     "https://www.wired.com/feed/category/business/latest/rss": "#tech #business",
-                     "http://feeds.arstechnica.com/arstechnica/technology-lab": "#tech",
-                     "http://feeds.arstechnica.com/arstechnica/business": "#tech #business",
-                     "http://feeds.arstechnica.com/arstechnica/security": "#security #hacktivism",
-                     "http://feeds.arstechnica.com/arstechnica/tech-policy": "#tech #law",
-                     "https://krebsonsecurity.com/feed/": "#security #data",
-                     "https://www.hackread.com/sec/":"#security #data #hacking",
-                     "https://www.hackread.com/sec/malware/":"#security #malware",
-                     "https://www.hackread.com/latest-cyber-crime/":"#security #hacking #cybercrime",
-                     "https://www.hackread.com/latest-cyber-crime/phishing-scam/":"#security #phishing #cybercrime",
-                     "https://www.hackread.com/how-to/": "#lifehack #tech #howto",
-                     "https://www.hackerone.com/blog/category/Data%20and%20Analysis":"#data #analysis",
-                     "https://www.hackread.com/cyber-events/cyber-attacks-cyber-events/":"#security #cyberattack #data",
-                     "https://www.hackread.com/cyber-events/censorship/": "#censorship",
-                     "https://www.hackread.com/surveillance/":"#surveillance",
-                     "http://starbridgepartners.com/data-science-report/":"#data #DataScience #DataEngineering",
-                     "https://www.smartdatacollective.com/feed/":"#data #DataScience #DataEngineering",
-                     "https://insidebigdata.com/feed/":"#bigdata #data #DataScience #DataEngineering",
-                     "https://simplystatistics.org/":"#data #statistics #DataScience #DataEngineering",
-                     "https://101.datascience.community/":"#data #DataScience #DataEngineering",
-                     "https://dataconomy.com/":"#data #DataScience #DataEngineering",
-                     "https://www.hackread.com/surveillance/drones/":"#drone #security #surveillance",
-                     "https://www.hackread.com/surveillance/nsa/": "#nsa #security #surveillance",
-                     "https://www.hackread.com/surveillance/privacy/":"#privacy #security #surveillance"
-                     }
+    tweetDict = {"http://feeds.feedburner.com/TechCrunchIT": "#tech",
+                    "http://feeds.feedburner.com/TechCrunch/startups": "#startup",
+                    "http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml": "#tech",
+                    "https://www.wired.com/feed/category/security/latest/rss": "#security",
+                    "https://www.wired.com/feed/rss": "#tech #news",
+                    "https://www.wired.com/feed/category/science/latest/rss": "#science",
+                    "https://www.wired.com/feed/category/business/latest/rss": "#tech #business",
+                    "http://feeds.arstechnica.com/arstechnica/technology-lab": "#tech",
+                    "http://feeds.arstechnica.com/arstechnica/business": "#tech #business",
+                    "http://feeds.arstechnica.com/arstechnica/security": "#security #hacktivism",
+                    "http://feeds.arstechnica.com/arstechnica/tech-policy": "#tech #law",
+                    "https://krebsonsecurity.com/feed/": "#security #data",
+                    "https://www.hackread.com/sec/":"#security #data #hacking",
+                    "https://www.hackread.com/sec/malware/":"#security #malware",
+                    "https://www.hackread.com/latest-cyber-crime/":"#security #hacking #cybercrime",
+                    "https://www.hackread.com/latest-cyber-crime/phishing-scam/":"#security #phishing #cybercrime",
+                    "https://www.hackread.com/how-to/": "#lifehack #tech #howto",
+                    "https://www.hackerone.com/blog/category/Data%20and%20Analysis":"#data #analysis",
+                    "https://www.hackread.com/cyber-events/cyber-attacks-cyber-events/":"#security #cyberattack #data",
+                    "https://www.hackread.com/cyber-events/censorship/": "#censorship",
+                    "https://www.hackread.com/surveillance/":"#surveillance",
+                    "http://starbridgepartners.com/data-science-report/":"#data #DataScience #DataEngineering",
+                    "https://www.smartdatacollective.com/feed/":"#data #DataScience #DataEngineering",
+                    "https://insidebigdata.com/feed/":"#bigdata #data #DataScience #DataEngineering",
+                    "https://simplystatistics.org/":"#data #statistics #DataScience #DataEngineering",
+                    "https://101.datascience.community/":"#data #DataScience #DataEngineering",
+                    "https://dataconomy.com/":"#data #DataScience #DataEngineering",
+                    "https://www.hackread.com/surveillance/drones/":"#drone #security #surveillance",
+                    "https://www.hackread.com/surveillance/nsa/": "#nsa #security #surveillance",
+                    "https://www.hackread.com/surveillance/privacy/":"#privacy #security #surveillance"
+                    }
 
     feedRow = (random.choice(list(tweetDict.items())))
     RssFeedURL = feedRow[0]
@@ -140,6 +142,7 @@ if __name__ == "__main__":
     if randomNumber <= 38:
         sleepTime = randomNumber * 30
         currentHour = datetime.datetime.now().hour
+        print("Current Hour: {}".format(currentHour))
         if currentHour >= startTime and currentHour <= endTime:
             print("Posting")
             # time.sleep(sleepTime)
