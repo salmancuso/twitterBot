b7998876805f14d2b38a6cca47eb73f212802d5a
Sal Mancuso <smancuso@stanford.edu>
2020-03-02 21:13:20 -0800

    teesting

--- sandbox3.py
+++ sandbox3.py
@@ -5,7 +5,7 @@ def pwdDir ():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     return dir_path
 
-
+tweetRssLog = []
 with open(str('{}/tweetBotLogger.csv'.format(pwdDir())), 'r') as tweetLog:
     tweetLogFile = csv.reader(tweetLog, delimiter=',', quotechar='"')
     print (tweetLogFile)
