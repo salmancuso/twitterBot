86ccd334188461acd8f7cd0dccfd21dc8eb90375
Sal Mancuso <smancuso@stanford.edu>
2020-03-05 13:35:53 -0800

    updated Rss Feed List

--- sandbox2.py
+++ sandbox2.py
@@ -1,6 +1,6 @@
 import feedparser
     
-feedURL = str("http://hackerone.com/blog.rss")
+feedURL = str("https://gizmodo.com/rss")
 feedData = feedparser.parse(feedURL)
 if feedData[ "bozo" ] == 0: ## if 0, then it is a good feed.
     print("Feed is good")
