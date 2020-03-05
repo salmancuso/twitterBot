5114303c35f08d7f3b4cdc82cb0efba86b9dc551
Sal Mancuso <smancuso@stanford.edu>
2020-03-05 13:14:57 -0800

    updated feed list

--- sandbox2.py
+++ sandbox2.py
@@ -1,11 +1,8 @@
-http://feeds.feedburner.com/TechCrunchIT,#tech
-http://feeds.feedburner.com/TechCrunch/startups,#startup
-http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml,#tech
-https://www.wired.com/feed/category/security/latest/rss,#security
-https://www.wired.com/feed/rss,#tech #news
-https://www.wired.com/feed/category/science/latest/rss,#science
-https://www.wired.com/feed/category/business/latest/rss,#tech #business
-https://krebsonsecurity.com/feed,#security #data
-https://www.smartdatacollective.com/feed,#data #DataScience #DataEngineering
-https://insidebigdata.com/feed,#bigdata #data #DataScience #DataEngineering
-https://simplystatistics.org/index.xml,#data #statistics #DataScience #DataEngineering
+import feedparser
+    
+feedURL = str("http://hackerone.com/blog.rss")
+feedData = feedparser.parse(feedURL)
+if feedData[ "bozo" ] == 0: ## if 0, then it is a good feed.
+    print("Feed is good")
+else:
+    print("bad feed")
