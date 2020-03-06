9e704b7401d8179d1d115a275f34991538903a2b
Sal Mancuso <smancuso@stanford.edu>
2020-03-06 09:38:39 -0800

    udpated feeds

--- sandbox2.py
+++ sandbox2.py
@@ -1,6 +1,6 @@
 import feedparser
     
-feedURL = str("http://feeds.feedburner.com/FeaturedBlogPosts-DataScienceCentral")
+feedURL = str("https://rss.bizjournals.com/feed/4d3b75f3af9d6c56af49b9e8a79ff847173c75c4/3033?market=sanfrancisco,sanjose&selectortype=channel&selectorvalue=15")
 feedData = feedparser.parse(feedURL)
 if feedData[ "bozo" ] == 0: ## if 0, then it is a good feed.
     print("Feed is good")
